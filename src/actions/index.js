99bd8e9ceac587d26f4b774482c53cf96c4a185c
Sheri Wilson <swilson@simmonsresearch.com>
2016-07-23 13:15:33 -0700

    adds google maps

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,6 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APIKEY=${API_K
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -29,7 +29,7 @@ class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-          placeholder="Get a 5-day forecast in your favorite cities"
+          placeholder="Get a 5-day average forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange}
